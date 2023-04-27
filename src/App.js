6a656102701199908a4dad661b9f7e6a139b9b27
jrwesch <jrwesch@gmail.com>
2023-04-27 12:34:44 -0400

    changed the file tree to better group pages together, should be more in line with the grading criteria

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import './App.css';
-import About from './components/About';
-import Nav from './components/Nav';
+import Portfolio from "./components/Portfolio"
 
-function App() {
-  return (
-    <div>
-      <Nav></Nav>
-      <main>
-        <About></About>
-      </main>
-    </div>
-  );
-}
+const App = () => <Portfolio/>
 
 export default App;
