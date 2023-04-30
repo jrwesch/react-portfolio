e517665ebe2a60280e61ae5025b1c0b953798323
jrwesch <jrwesch@gmail.com>
2023-04-30 11:58:05 -0400

    added front end functionality to contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,40 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import {validateEmail} from '../../utils/helpers';
 
-const Contact = () => (
+const Contact = () => {
+
+    const [formState, setFormState] = useState({name: '', email: '', message: ''});
+    const [errorMessage, setErrorMessage] = useState('');
+    const {name, email, message} = formState;
     
-    <div>
-        <h1>Contact Form</h1>
+   function handleChange(e) {
+    if (e.target.name === 'email') {
+        const isValid = validateEmail(e.target.value);
+        console.log(isValid);
+        if (!isValid){
+            setErrorMessage('Please enter a valid email');
+        } else {
+            setErrorMessage('');
+        }
+    } else {
+        if (!e.target.value.length){
+            setErrorMessage(`${e.target.name} is required`);
+        } else {
+            setErrorMessage('');
+        }
+    }
+    if (!errorMessage) {
+        setFormState({...formState, [e.target.name]: e.target.value});
+    }
+   }; 
 
-        <p>
-        Black spot Gold Road Sea Legs killick pillage barque stern schooner. Long boat 
-        Jack Tar run a shot across the bow Jack Ketch execution dock spyglass carouser 
-        hardtack. Flogging fathom black jack scuppers barque squiffy Jack Ketch lass. Bilge 
-        rat chandler bounty port Letter of Marque gangway mutiny killick. Take a caulk 
-        starboard belaying pin Corsair ahoy snow sheet cable.
+};
 
-        Sea Legs Jolly Roger scuppers tender square-rigged pirate booty chase. Interloper 
-        Buccaneer stern Yellow Jack port rutters log hornswaggle. Belay heave down provost 
-        fluke swab yawl coffer Sea Legs. Mutiny execution dock topgallant brigantine pressgang 
-        log to go on account scuttle. List cutlass Jolly Roger weigh anchor hardtack salmagundi 
-        avast swab.
 
-        Boom hang the jib code of conduct matey spanker transom to go on account tender. 
-        Furl bucko topgallant chantey mutiny brigantine no prey, no pay Sail ho. Jack Ketch Nelsons 
-        folly rutters red ensign scallywag lookout black spot man-of-war. Rum holystone grog gally 
-        stern scurvy yardarm fire in the hole. Jack Ketch piracy Barbary Coast chase guns rope's 
-        end wench cog barque.
-
-        Skysail tender deadlights capstan long clothes Sink me salmagundi Pirate Round. Hulk schooner 
-        main sheet cackle fruit gaff jolly boat deadlights red ensign. Gangplank fluke gun Pirate 
-        Round list run a rig spyglass knave. Mizzen execution dock lass splice the main brace blow 
-        the man down ahoy dead men tell no tales black spot. Clipper Pieces of Eight Sea Legs 
-        yardarm carouser chase guns furl nipper.
-
-        </p>
-
-    </div>
-    
-
-);
 
 export default Contact;
