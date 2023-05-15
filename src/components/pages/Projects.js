ca62142a3f7b3850362f5c009381bd05fd671f63
jrwesch <jrwesch@gmail.com>
2023-05-15 18:38:03 -0400

    added final group project

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -29,6 +29,13 @@ const projects = [
         gitURL: "https://github.com/jrwesch/readme-generator",
         tech: "HTML/CSS/JavaScript/Node.js",
         style: "readme-generator"   
+    },
+    {
+        name: "UFit Final Project",
+        liveURL: "https://pure-savannah-65137.herokuapp.com/",
+        gitURL: "https://github.com/sampaul10/Fitness-Tracker",
+        tech: "",
+        style: "UFit"
     }
     
 
